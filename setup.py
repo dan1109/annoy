8e0f12416451e64777f1dc5171003dc612facf13
Fariz Rahman <farizrahman4u@gmail.com>
2017-05-25 02:22:35 +0530

    bug fix?

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ setup(name='annoy',
         Extension(
             'annoy.annoylib', ['src/annoymodule.cc'],
             depends=['src/annoylib.h', 'src/kissrandom.h', 'src/mman.h'],
-            extra_compile_args=['-O3', '-march=native', '-ffast-math'] + travis_extra_compile_args,
+            extra_compile_args=['-O3', '-ffast-math'] + travis_extra_compile_args,
         )
       ],
       long_description=long_description,
